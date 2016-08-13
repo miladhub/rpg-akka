42607ac9d55e1d970b9bd1f41be066aa8c92a442
Milad Bourhani <milad.bourhani@gmail.com>
2016-08-13 13:20:33 +0200

    split tests

---
+++ src/test/scala/CharacterSessionSpec.scala
@@ -0,0 +1,30 @@
+import CharacterSession.{CharacterResponse, WhoAmI}
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class CharacterSessionSpec(_system: ActorSystem)
+  extends TestKit(_system)
+    with WordSpecLike
+    with ImplicitSender
+    with Matchers
+    with BeforeAndAfterAll {
+
+  def this() = this(ActorSystem("AkkaTcpRpg"))
+
+  override def afterAll: Unit = {
+    Await.result(system.terminate(), 10.seconds)
+  }
+
+  "the character session" should {
+    "say who the character is" in {
+      val charSession = system.actorOf(CharacterSession.props("John"))
+
+      charSession ! WhoAmI
+      expectMsg(CharacterResponse("Your name is John."))
+    }
+  }
+}
--- src/test/scala/UserSessionSpec.scala
+++ src/test/scala/UserSessionSpec.scala
@@ -1,6 +1,6 @@
-import akka.actor.{ActorSystem, Props}
+import akka.actor.ActorSystem
 import akka.io.Tcp.{Close, Received, Write}
-import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
+import akka.testkit.{ImplicitSender, TestKit}
 import akka.util.ByteString
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
@@ -20,36 +20,36 @@ class UserSessionSpec(_system: ActorSystem)
     Await.result(system.terminate(), 10.seconds)
   }
 
-  "the client handler" should {
+  "the user session" should {
     "close the connection when saying bye" in {
-      val handler = system.actorOf(UserSession.props(testActor))
+      val userSession = system.actorOf(UserSession.props(testActor))
 
-      handler ! Received(ByteString("bye."))
+      userSession ! Received(ByteString("bye."))
       expectMsg(Write(ByteString("> Bye!\n")))
       expectMsg(Close)
     }
 
     "welcome characters as they enter" in {
-      val handler = system.actorOf(UserSession.props(testActor))
+      val userSession = system.actorOf(UserSession.props(testActor))
 
-      handler ! Received(ByteString("enter as John\n"))
+      userSession ! Received(ByteString("enter as John\n"))
       expectMsg(Write(ByteString("> Welcome, John!\n")))
     }
 
     "say what to unknown commands" in {
-      val handler = system.actorOf(UserSession.props(testActor))
+      val userSession = system.actorOf(UserSession.props(testActor))
 
-      handler ! Received(ByteString("foobar\n"))
+      userSession ! Received(ByteString("foobar\n"))
       expectMsg(Write(ByteString("> I'm sorry, what?\n")))
     }
 
     "remember the character" in {
-      val handler = system.actorOf(UserSession.props(testActor))
+      val userSession = system.actorOf(UserSession.props(testActor))
 
-      handler ! Received(ByteString("enter as John\n"))
+      userSession ! Received(ByteString("enter as John\n"))
       expectMsg(Write(ByteString("> Welcome, John!\n")))
 
-      handler ! Received(ByteString("who am i\n"))
+      userSession ! Received(ByteString("who am i\n"))
       expectMsg(Write(ByteString("> Your name is John.\n")))
     }
   }
